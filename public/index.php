30f55b982de7874de96b247fa6e5fdfb8df7d603
hxtree <matthew.heroux@gmail.com>
2023-04-19 22:01:40 -0500

    fix: remove comments

--- public/index.php
+++ public/index.php
@@ -15,24 +15,17 @@ use Hxtree\Json2Resume\Resume;
 
 define('FPDF_FONTPATH', __DIR__ . '/font');
 
-// create resume object
 $pdf = new Resume();
 
-// load fonts
 $pdf->AddFont('PragatiNarrow', '', 'PragatiNarrow-Regular.php');
 $pdf->AddFont('PragatiNarrow', 'B', 'PragatiNarrow-Bold.php');
 $pdf->AddFont('PragatiNarrow', 'I', 'OpenSans-LightItalic.php');
 $pdf->AddFont('San', 'B', 'archivob.php');
 
-// init page
 $pdf->SetMargins($pdf->margin['left'], $pdf->margin['top'], $pdf->margin['right']);
+
 $pdf->AddPage();
-
-// render career profile section
 $pdf->CareerProfile();
-
-// render other sections
 $pdf->Sections();
 
-// output PDF
 $pdf->Output();
