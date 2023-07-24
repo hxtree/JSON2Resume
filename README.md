403e400f7cac4b34192ed98099a304bb3def534b
hxtree <matthew.heroux@gmail.com>
2023-07-24 12:14:25 -0500

    feat: add info about master resume

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/d96205a06e2a45838b782cc23b07ea95)](https://app.codacy.com/gh/hxtree/JSON2Resume/)
 
-JSON2Resume converts a JSON array into a PDF resume. This separates the presentation layer from data allowing for autogenerate PDF resumes.
+JSON2Resume is a powerful tool that converts a JSON array into a PDF resume. The unique feature of this tool is that it is based on the highly successful resume format used by Barack Obama. By leveraging this proven format, JSON2Resume ensures that your resume follows a well-structured and effective layout that has been recognized for its impact.
+
+The tool excels at separating the presentation layer from the data, allowing for the seamless autogeneration of PDF resumes. You can provide your resume content in a structured JSON format, and JSON2Resume will handle the formatting and layout to produce a professional-looking PDF resume. This format not only simplifies the resume creation process but also ensures that your qualifications and experiences are presented in a compelling manner, increasing your chances of making a strong impression on potential employers.
+
+With JSON2Resume, you can effortlessly customize and update your resumes, tailoring them to different job opportunities while maintaining the successful format used by Barack Obama. This approach streamlines the entire resume-building process, giving you the confidence that your application stands out and effectively showcases your skills and achievements to prospective employers.
+
+## Best Practices 
+
+### Master Resume
+
+You are advised to prepare a Master Resume, which should be kept private and contain all your achievements, skills, work history, education, and other relevant details. This document serves as a comprehensive record of your professional background. To ensure its safety, store this file with redundancy to account for data failures, as it is a dynamic and constantly updated document.
+
+The Master Resume typically spans 4 to 6 pages. Creating and maintaining this file will prove beneficial, as it will significantly save you time when generating your Targeted/Customized Resume tailored for specific job applications.
+
+### Targeted/Customized Resume
+
+The Master Resume serves as your reference for creating a Targeted/Customized resume. When applying for a potential job posting, you will use the Targeted/Customized resume. This version is specifically tailored to highlight the most relevant skills, experiences, and achievements that match the requirements of the job you are applying for. By copying and pasting relevant information from the Master Resume, you can ensure that your application aligns closely with the specific job opportunity, increasing your chances of landing the position.
 
 ![Resume Example](https://github.com/hxtree/JSON2Resume/raw/master/docs/example.png "Screenshot")
 
--- src/Resume.php
+++ src/Resume.php
@@ -67,7 +67,7 @@ class Resume extends \FPDF
 
         $this->ln(2);
 
-        $this->column_width = $this->content_width / 3 - 1;
+        $this->column_width = $this->content_width / 4 - 1;
         $count = 0;
 
         foreach ($this->data->social as $social) {
@@ -78,11 +78,11 @@ class Resume extends \FPDF
             $this->SetX($x_pos + 4); // TODO ?
 
             // print email
-            $this->SetFont('PragatiNarrow', '', 11);
+            $this->SetFont('PragatiNarrow', '', 12);
             @$this->Cell($this->column_width, 5, $social->value, 0, 0, 'L', 0, $this->data->email);
 
             ++$count;
-            if (0 == $count % 3) {
+            if (0 == $count % 4) {
                 $this->ln();
             }
         }
@@ -100,7 +100,7 @@ class Resume extends \FPDF
         $this->SetFont('San', 'B', 16);
         $this->Cell($this->content_width / 2, 6, $this->data->name, 0, 0, 'L');
 
-        $this->SetFont('PragatiNarrow', '', 11);
+        $this->SetFont('PragatiNarrow', '', 12);
         $this->Cell($this->content_width / 2, 6, 'Page '.$this->page_count, 0, 0, 'R');
         $this->ln();
         $this->ThickLine('up');
@@ -147,8 +147,8 @@ class Resume extends \FPDF
     {
         $this->SetFont('San', 'B', 12);
         $this->MultiCell($this->content_width, 6, 'CAREER PROFILE:  ', 0, 'C');
-        $this->SetFont('PragatiNarrow', '', 10);
 
+        $this->SetFont('PragatiNarrow', '', 12);
         foreach ($this->data->profile as $sentence) {
             $this->MultiCell($this->content_width, 5, $sentence, 0, 'J');
             $this->ln(4);
@@ -182,7 +182,7 @@ class Resume extends \FPDF
             $length = count($section->list);
             $counter = 1;
             foreach ($section->list as $array) {
-                $this->SetFont('PragatiNarrow', '', 11);
+                $this->SetFont('PragatiNarrow', '', 12);
                 $this->Cell($this->indent, 4, chr(149), '', 0, 'R');
                 $this->Cell($this->column_width - $this->indent, 4, $array, '', 0, 'L');
                 if (1 == $counter) {
@@ -198,9 +198,9 @@ class Resume extends \FPDF
         }
 
         foreach ($section->array as $section2) {
-            $this->SetFont('PragatiNarrow', 'B', 10);
+            $this->SetFont('PragatiNarrow', 'B', 12);
             $this->BulletItem(1, ' ', $section2->title ?? '', $section2->date ?? '');
-            $this->SetFont('PragatiNarrow', 'I', 8.5);
+            $this->SetFont('PragatiNarrow', 'I', 9);
             $this->BulletItem(1, ' ', $section2->description);
             $this->ln(0.4);
             if (!property_exists($section2, 'array') || !is_array($section2->array)) {
@@ -208,18 +208,17 @@ class Resume extends \FPDF
             }
 
             foreach ($section2->array as $section3) {
-                $this->SetFont('PragatiNarrow', 'B', 10);
+                $this->SetFont('PragatiNarrow', 'B', 12);
                 $this->BulletItem(2, null, $section3->title ?? '', $section3->date ?? '');
                 if (!property_exists($section3, 'array')) {
                     $this->ln(0.4);
                     continue;
                 }
                 foreach ($section3->array as $section4) {
-                    $this->SetFont('PragatiNarrow', '', 10);
+                    $this->SetFont('PragatiNarrow', '', 12);
                     $this->BulletItem(3, '—', $section4);
                 }
                 if (isset($section3->technologies) && ($section3->technologies > 0)) {
-                    $this->SetFont('PragatiNarrow', '', 10);
                     $this->CondensedBulletList(2.5, 'Technologies:', $section3->technologies);
                 }
                 $this->ln(0.4);
@@ -245,16 +244,16 @@ class Resume extends \FPDF
         if (null !== $date) {
             $column_width = ($this->content_width - $this->indent * $indent_amount) / 2;
 
-            $this->SetFont('PragatiNarrow', 'B', 10);
+            $this->SetFont('PragatiNarrow', 'B', 12);
             $this->Cell($column_width, 4, iconv('UTF-8', 'windows-1252', $array), '', 0, 'L');
-            $this->SetFont('PragatiNarrow', '', 10);
+            $this->SetFont('PragatiNarrow', '', 12);
             $this->Cell($column_width, 4, $date, '', 0, 'R');
             $this->ln();
 
             return;
         }
 
-        $this->MultiCell(0, 4, iconv('UTF-8', 'windows-1252', $array), 0, 'J', false);
+        $this->MultiCell(0, 5, iconv('UTF-8', 'windows-1252', $array), 0, 'J', false);
 
         return;
     }
@@ -264,17 +263,19 @@ class Resume extends \FPDF
      */
     public function CondensedBulletList($indent_amount, $prefix, $array): void
     {
+        $this->SetFont('PragatiNarrow', 'I', 10);
+
         $char = iconv('UTF-8', 'windows-1252', chr(639));
-        $this->ln(0.05);
+        $this->ln(1);
         $indent_total = ($this->indent * ($indent_amount + 1)) - $this->indent;
         $this->Cell($indent_total, 4, '', '', 0, 'R');
-        $this->SetFont('PragatiNarrow', 'I', 8);
         $sentence = "{$prefix} ";
         foreach ($array as $item) {
             $item = iconv('UTF-8', 'windows-1252', $item);
-            $sentence .= "{$char} {$item} ";
+            $sentence .= "{$char}{$item}  ";
         }
 
         $this->MultiCell(0, 4, $sentence, 0, 'J', false);
+        $this->ln(1);
     }
 }
